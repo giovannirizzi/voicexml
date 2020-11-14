344a047dfc8c02a802a4e2cf9e328ed3f88224e9
Giovanni Rizzi <giovannirizzi98@live.com>
2020-11-14 21:34:29 +0100

    add webserver to serve vxml files

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const winston = require('winston');
 winston.cli();
 winston.level = process.env['LEVEL'] || 'debug';
 
+/* Start the web server to serve vxml files */
+var connect = require('connect')
+var serveStatic = require('serve-static');
+
+var app = connect();
+app.use(serveStatic("./test"));
+app.listen(9000);
+
+
 const session = new (require('./src/Session'))();
 
 const uri = process.argv[2] || 'http://localhost:9000/index.vxml';
